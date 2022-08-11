5dbdb454e3d01e76bcb3c8e7ce080050167b3d4e
exsto1 <exsto1@gmail.com>
2022-08-11 11:48:30 +0200

    .

--- runcommand.sh
+++ runcommand.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #SBATCH --job-name="FULL_TOPOLY_SIKORA"
 #SBATCH --gres=gpu
-source
+source $HOME/topology_AI_pipeline/venv/bin/activate
+python $HOME/topology_AI_pipeline/pipeline.py -i $HOME/topology_AI_pipeline/test_full.txt
